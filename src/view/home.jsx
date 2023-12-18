3d5077477935d004c6d76b046a0d732c08d24e89
LiedsonLB <liedson.b9@gmail.com>
2023-12-18 19:44:45 -0300

    responsive neoFranxxNews

--- src/view/home.jsx
+++ src/view/home.jsx
@@ -24,6 +24,7 @@ function HomePage() {
                 <div id="home">
                     <Aside {...profile}></Aside>
                     <div id="main-home">
+                        <Outlet/>
                         <NeoFranxxNews/>
                     </div>
                 </div>
