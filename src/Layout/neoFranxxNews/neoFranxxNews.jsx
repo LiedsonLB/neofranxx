3e582b8ca7c2b18dd08e0e08aa37e1edc61b01fc
LiedsonLB <liedson.b9@gmail.com>
2023-12-19 11:05:23 -0300

    add coins API

--- src/Layout/neoFranxxNews/neoFranxxNews.jsx
+++ src/Layout/neoFranxxNews/neoFranxxNews.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import '../neoFranxxNews/neoFranxxNews.css'
 
 const NeoFranxxNews = () => {
+    const [moedas, setMoedas] = useState(null);
+
+    useEffect(() => {
+        const apiUrl = 'https://v6.exchangerate-api.com/v6/26571734dacb7c59752a567e/latest/BRL';
+
+        fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const moedasArray = [];
+            for (const currency in data.conversion_rates) {
+                const rate = parseFloat(data.conversion_rates[currency])
+                .toLocaleString('pt-br', {
+                    style: 'currency',
+                    currency: 'BRL'
+                });
+                moedasArray.push({ currency, rate });
+            }
+            setMoedas(moedasArray);
+        })
+        .catch(error => console.error('Erro na solicitação:', error));
+    }, []);
+
   return (
     <main id="main-news">
         <div id="main-news-container">
@@ -11,7 +33,7 @@ const NeoFranxxNews = () => {
             <h4>Destaques</h4>
             <section id="destaques">   
                 <div className="major-news">
-                    <a href="">
+                    <a href="\home">
                         <div className="major-img">
                             <img src="/img/vista-frontal-do-corte-da-fita-vermelha-na-inauguracao-do-edificio_8353-10570.avif" alt="img-person"/>
                         </div>
@@ -22,7 +44,7 @@ const NeoFranxxNews = () => {
                 </div>
 
                 <div className="outhers-news">
-                    <a href="">
+                    <a href="\login">
                         <div className="secundary-news">
                             <div className="container-secundary-img-2"></div>
                             <div className="container-secundary-text">
@@ -30,7 +52,7 @@ const NeoFranxxNews = () => {
                             </div>
                         </div>
                     </a>
-                    <a href="">
+                    <a href="\login">
                         <div className="secundary-news">
                             <div className="container-secundary-img-1"></div>
                             <div className="container-secundary-text">
@@ -41,80 +63,16 @@ const NeoFranxxNews = () => {
                 </div>
             </section>
 
-            <h4>Cotações</h4>
+            <h4>Api de Cotações</h4>
             <section id="cotacoes-cards">
-                <div className="card-coin red">
-                    <div className="coin">
-                        <p><strong>R$</strong></p>    
+                {moedas && moedas.map((currency, index) => (
+                    <div key={index} className="card-coin">
+                        <div className="coin">
+                            <p><strong>{currency.currency}</strong></p>
+                        </div>
+                        <p>{currency.rate}</p>
                     </div>
-                    <p>16.00%</p>
-                </div>
-                <div className="card-coin green">
-                    <div className="coin">
-                        <p><strong>US</strong></p>    
-                    </div>
-                    <p>12.06%</p>
-                </div>
-                <div className="card-coin">
-                    <div className="coin">
-                        <p><strong>Bovespa</strong></p>    
-                    </div>
-                    <p>5.36%</p>
-                </div>
-                <div className="card-coin red">
-                    <div className="coin">
-                        <p><strong>R$</strong></p>    
-                    </div>
-                    <p>16.00%</p>
-                </div>
-                <div className="card-coin green">
-                    <div className="coin">
-                        <p><strong>US</strong></p>    
-                    </div>
-                    <p>12.06%</p>
-                </div>
-                <div className="card-coin">
-                    <div className="coin">
-                        <p><strong>Bovespa</strong></p>    
-                    </div>
-                    <p>5.36%</p>
-                </div>
-                <div className="card-coin red">
-                    <div className="coin">
-                        <p><strong>R$</strong></p>    
-                    </div>
-                    <p>16.00%</p>
-                </div>
-                <div className="card-coin green">
-                    <div className="coin">
-                        <p><strong>US</strong></p>    
-                    </div>
-                    <p>12.06%</p>
-                </div>
-                <div className="card-coin">
-                    <div className="coin">
-                        <p><strong>Bovespa</strong></p>    
-                    </div>
-                    <p>5.36%</p>
-                </div>
-                <div className="card-coin red">
-                    <div className="coin">
-                        <p><strong>R$</strong></p>    
-                    </div>
-                    <p>16.00%</p>
-                </div>
-                <div className="card-coin green">
-                    <div className="coin">
-                        <p><strong>US</strong></p>    
-                    </div>
-                    <p>12.06%</p>
-                </div>
-                <div className="card-coin">
-                    <div className="coin">
-                        <p><strong>Bovespa</strong></p>    
-                    </div>
-                    <p>5.36%</p>
-                </div>
+                ))}
             </section>
 
             <h4>Nossas Lojas</h4>
